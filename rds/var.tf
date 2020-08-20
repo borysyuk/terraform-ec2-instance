67ee26556b5556e22cb08d1c45281ab2aeb2123c
borysyuk <borysyuk.ihor@gmail.com>
2020-08-21 00:36:38 +0300

    Update var.tf

--- rds/var.tf
+++ rds/var.tf
@@ -1,7 +1,7 @@
 
 
 variable "primary_region" {
-  description = "AWS region to launch primary db instances for global database"
+  description = "AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database"
   default     = "us-east-1"
 }
 
@@ -11,20 +11,20 @@ variable "secondary_region" {
 }
 
 variable "aurora_instance_class" {
-  default = "db.t2.small"
+  default = "db.t2.small AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database"
 }
 
 variable "first_subnet_id" {
-  description = "VPC Subnet into which to launch your db instance"
+  description = "VPC Subnet into which to launch your db instance AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database"
   default     = "subnet-3d82d101"
 }
 
 variable "second_subnet_id" {
-  description = "VPC Subnet into which to launch your db instance"
+  description = "VPC Subnet into which to launch your db instance AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database  AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database"
   default     = "subnet-b347fefb"
 }
 
 variable "third_subnet_id" {
-  description = "VPC Subnet into which to launch your db instance"
+  description = "VPC Subnet into which to launch your db instance AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database AWS region to launch primary db instances for global database"
   default     = "subnet-7e3fd71a"
 }
